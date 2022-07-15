7aa9f563235c9ec99768360b87d21a82576b5b17
MB-Tiger <mohammadrezabafekr8282@gmail.com>
2022-07-15 11:42:06 +0430

    comment don

--- src/comment/controller.js
+++ src/comment/controller.js
@@ -5,39 +5,40 @@ import UID from "lib/utils/UID";
 export default {
   createComment: async (req, res) => {
     try {
+      if (!req.body.text || !req.body.productId)
+        throw new Error("bad request: bad inputs");
 
-      if (!req.body.text || !req.body.productId) throw new Error('bad request: bad inputs')
-      
-      const thisProduct = await Product.findById(req.body.productId)
+      const thisProduct = await Product.findById(req.body.productId);
 
-      if (!thisProduct || !thisProduct._id) throw new Error('bad request: no such product found')
+      if (!thisProduct || !thisProduct._id)
+        throw new Error("bad request: no such product found");
 
       await Comment.create({
         ...req.body,
-        userId: UID("FakeUser")
-      })
+        userId: UID("FakeUser"),
+      });
 
-      return res.status(200).json({ msg: 'ok' })
-      
+      return res.status(200).json({ msg: "ok" });
     } catch (error) {
       return res.status(500).json({ msg: error.message });
     }
   },
   getComment: async (req, res) => {
     try {
+      const thisProduct = await Product.findById(String(req.params.id));
 
-      const thisProduct = await Product.findById(String(req.params._id))
+      console.log(req.params);
 
-      if (!thisProduct || !thisProduct._id) throw new Error('bad request: no such product found')
+      if (!thisProduct || !thisProduct._id)
+        throw new Error("bad request: no such product found");
 
-      const thisComment = deepClone(await Comment.getProductComments(thisProduct._id))
+      const thisComment = deepClone(
+        await Comment.getProductComments(thisProduct._id)
+      );
 
-      thisComment.forEach((item, i) => item)
-  
-      return res.status(200).json(thisComment)
-      
+      return res.status(200).json(thisComment);
     } catch (error) {
-      return res.json({ msg: error.message })
+      return res.json({ msg: error.message });
     }
-  }
-}
+  },
+};
